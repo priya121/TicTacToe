4fdf59ec01253582782042291721db292afb1e62
priya121 <priya121@github.com>
2016-02-17 12:22:42 +0000

    Cleaned Human Player class by removing valid digit method.

--- src/main/java/ttt/player/HumanPlayer.java
+++ src/main/java/ttt/player/HumanPlayer.java
@@ -6,36 +6,30 @@ import ttt.inputOutput.DigitValidator;
 import ttt.inputOutput.IO;
 
 public class HumanPlayer implements Player {
+    private DigitValidator digitValidator;
     private IO io;
-    private int indexChosen;
-    private String userInput;
     private Symbol symbol;
 
     public HumanPlayer(IO io, Symbol symbol) {
         this.io = io;
         this.symbol = symbol;
+        this.digitValidator = new DigitValidator();
     }
 
     public String takesUserInput() {
-        userInput = io.takeInput();
-        return userInput;
+        return io.takeInput();
     }
 
     private int validMove(Board board) {
-        while (!checksDigit(userInput) || !board.isPositionEmpty(userInput)) {
+        String userInput = takesUserInput();
+        while (!digitValidator.check(userInput) || !board.isPositionEmpty(userInput)) {
             io.showOutput("That position is already taken or is not on the board, try again.");
-            takesUserInput();
+            userInput = takesUserInput();
         }
         return Integer.parseInt(userInput);
     }
 
-    public boolean checksDigit(String userInput) {
-        DigitValidator digitValidator = new DigitValidator();
-        return digitValidator.check(userInput);
-    }
-
     public int move(Board board) {
-        takesUserInput();
         return validMove(board);
     }
 
